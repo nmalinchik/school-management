1cdf3afce589b9bb49d3c1bc4246517bc6c7289d
Mikalai Malinchyk <mikalai_malinchyk@epam.com>
2022-11-17 12:25:02 +0400

    init commit

---
+++ src/test/java/com/mallinapps/schoolmanagement/SchoolManagementBeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mallinapps.schoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
